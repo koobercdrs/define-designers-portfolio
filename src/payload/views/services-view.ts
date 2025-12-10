62bf7880cf16851805c136dabdb3e1facb0c2e53
gzautashvilii <gocha.zautashvili@itstep.ge>
2025-12-11 02:35:06 +0400

    Update services-view.ts

--- src/payload/views/services-view.ts
+++ src/payload/views/services-view.ts
@@ -1,7 +1,15 @@
 import { GlobalConfig } from 'payload'
+import { revalidatePath } from 'next/cache';
 
 export const ServiceView: GlobalConfig = {
   slug: 'service-view',
+    hooks: {
+    afterChange: [
+      async () => {
+        revalidatePath('/', 'layout');
+      },
+    ],
+  },
   fields: [
     // Hero
     {
